15cb13eba10522af889c15a46bdd261fbdfbc68d
Andrew Kuriger <a.kuriger@soul-dev.com>
2023-04-04 14:26:49 -0500

    Added function to splice specific items from an array when supplied as an arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(origArray, ...itemsToRemove) {
+    itemsToCheck = itemsToRemove.length;
+    let newArray = [];
 
+    for (let i = 0; i < itemsToCheck; i++) {
+        let index = origArray.indexOf(itemsToRemove[i])
+        if (index > -1) {
+            /* here we need to remove the item from the index at location index */
+
+            origArray.splice(index, 1);
+        }
+    }
+    
+    return origArray;
 };
 
 // Do not edit below this line
