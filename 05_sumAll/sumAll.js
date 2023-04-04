3dac615085d11ef4e2f1ca7a5d62e86338d9e962
Andrew Kuriger <a.kuriger@soul-dev.com>
2023-04-04 16:00:30 -0500

    Created a function to determine the sum of the middle of two numbers, including the two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
 
+    if (low < 0 || high < 0) {
+        console.log("ERROR");
+        return "ERROR";
+    }
+    else if (Number.isInteger(low) !== true || Number.isInteger(high) !== true) {
+        console.log("ERROR");
+        return "ERROR";
+    }
+
+    sum = 0;
+    if (low > high) {
+
+        while (high <= low) {
+            sum += high;
+            high++;
+        }
+
+    }
+    else {
+        while (low <= high) {
+            sum += low;
+            low++;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
