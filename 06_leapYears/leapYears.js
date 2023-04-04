1c649fd0073f6c781e8ae6cb021dbe043926a6df
Andrew Kuriger <a.kuriger@soul-dev.com>
2023-04-04 17:43:15 -0500

    Created a function to check for a leap year, if so, return true, else, return false

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
 
+    if (testYear % 4 == 0 && testYear % 100 !== 0) {
+        // Continue Testing
+        return true;
+    }
+    else if (testYear % 100 == 0 && testYear % 400 == 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
