955395fd6894b619d6bc9e89115647eff7eb81d6
Andrew Kuriger <a.kuriger@soul-dev.com>
2023-04-04 11:10:40 -0500

    Completed 02 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+let newstring = "";
+if (number < 0) {
+    return "ERROR"
+}
+for (i = 0; i < number; i++) {
+    newstring += string;
+}
+return newstring;
 };
 
 // Do not edit below this line
