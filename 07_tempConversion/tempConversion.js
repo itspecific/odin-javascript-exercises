019724b38bf29e2d6008e29d29a64fd8d9c4bfa8
Andrew Kuriger <a.kuriger@soul-dev.com>
2023-04-04 19:13:11 -0500

    Created two functions to convert to fahrenheit and celsius and round to closest decimal.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  floatnumber = (f - 32) * 5 / 9;
+
+  return Math.round(floatnumber*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  floatnumber = (c * 1.8) + 32;
+
+  return Math.round(floatnumber*10)/10;
 };
 
 // Do not edit below this line
